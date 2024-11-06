6b48d9445463861b19bde33f7cd5933474be6dd4
Cynthia Borot <cynthia.borot@vanoise-parcnational.fr>
2024-11-06 14:09:21 +0100

    feat (media) : Debug

--- js/media.js
+++ js/media.js
@@ -1,14 +1,20 @@
 async function addMedia2Json(speciesDict) {
 	console.log("addMedia2Json");
 	for(key in speciesDict) {
-		url = 'https://api.gbif.org/v1/species/' + speciesDict[key]["gbifId"] + '/media';
-		console.log(url + "<br>");
+		url = 'https://api.gbif.org/v1/species/' + speciesDict[key].gbifId + '/media';
+
 		await fetch(url,{method:'GET'})
 		.then(function(response) {return response.json(); })
 		.then(function(json) {
-			speciesDict[key]["media"] = json.results[0].identifier
+			// Media checking
+			if (json.results.length > 0) {
+				speciesDict[key].media = json.results[0].identifier;
+				console.log(speciesDict[key].media + "<br>");
+			}
 		});
+		console.log(key + "<br>");
+		console.log(url + "<br>");
 	}
-	console.log(speciesDict);
+
 	return speciesDict;
 }
