f6d294e1deadde00bbab270f8ba9359992021972
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-11-07 17:07:45 +0100

    feat(display): update

--- js/display.js
+++ js/display.js
@@ -1,57 +1,75 @@
-
 function displayTaxonsCard(taxonList) {
-    var myContainer = document.getElementById("taxons-results");
-    // proper call to the function
-    taxonList.forEach((el) => {
-        addTaxon(el, myContainer);
-    });
+  var myContainer = document.getElementById("taxons-results");
+  // proper call to the function
+  taxonList.forEach((el) => {
+    addTaxon(el, myContainer);
+  });
 }
-
 
 function addTaxon(arrayTaxon, refContainer) {
-    // from an array of dicts describing taxons, populates "table_contenu" with cards describing each
-    // get image from taxon
-    let imgTaxon = arrayTaxon["mediaUrl"];
+  // from an array of dicts describing taxons, populates "table_contenu" with cards describing each
+  // get image from taxon
+  let imgTaxon = arrayTaxon["mediaUrl"];
 
-    // Create card element to be appened to card-group
-    let cardCont = document.createElement("div");
-    cardCont.className = "card col cursor-pointer";
-    let myCard = document.createElement("div");
-    myCard.classList = "card-body";
+  // Create card element to be appened to card-group
+  let cardCont = document.createElement("div");
+  cardCont.className = "card col cursor-pointer";
+  let myCard = document.createElement("div");
+  myCard.classList = "card-body text-center";
 
-    let myCardFoot = document.createElement("div");
-    myCardFoot.classList = "card-footer";
+  let myCardFoot = document.createElement("div");
+  myCardFoot.classList = "card-footer";
 
-    // set title and text
-    let cardTitle = document.createElement("h5");
-    cardTitle.innerText = arrayTaxon["species"];
-    cardTitle.className = "card-title";
+  // set title and text
+  let cardTitle = document.createElement("h5");
+  cardTitle.innerText = arrayTaxon["species"];
+  cardTitle.className = "card-title";
 
-    let myFigure = document.createElement("img");
-    myFigure.id = arrayTaxon["species"];
-    myFigure.src = arrayTaxon["mediaUrl"];
-    myFigure.className = "card-img-top";
+  let myFigure = document.createElement("img");
+  myFigure.id = arrayTaxon["species"];
+  myFigure.src = arrayTaxon["mediaUrl"]
+    ? arrayTaxon["mediaUrl"]
+    : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+  myFigure.style.max_height = "200px";
+  myFigure.style.width = "80%";
 
-    let myStatus = document.createElement("small");
-    myStatus.innerText = arrayTaxon["status"];
-    myStatus.className = "text-muted";
+  let myStatus = document.createElement("small");
+  let html_ = "";
+  console.log(arrayTaxon.status);
+  Object.keys(arrayTaxon.status[0]).forEach((el) => {
+    let color =
+      arrayTaxon.status[0][el] in colorStatus
+        ? colorStatus[arrayTaxon.status[0][el]]
+        : "rgba(var(--bs-primary-rgb)";
+    if (arrayTaxon.status[0][el] && config.STATUS_LABELS[el]) {
+      html_ += `<span class="badge" style="background-color:${color}">${config.STATUS_LABELS[el]}: ${arrayTaxon.status[0][el]}</span> `;
+    }
+  });
+  myStatus.innerHTML = html_;
+  myStatus.className = "text-muted";
 
-    // Build elements
-    myCardFoot.appendChild(myStatus);
+  // Build elements
+  myCardFoot.appendChild(myStatus);
 
-    myCard.appendChild(myFigure);
-    myCard.appendChild(cardTitle);
-    cardCont.appendChild(myCard);
-    cardCont.appendChild(myCardFoot);
-    refContainer.appendChild(cardCont);
-    return;
+  myCard.appendChild(myFigure);
+  myCard.appendChild(cardTitle);
+  cardCont.appendChild(myCard);
+  cardCont.appendChild(myCardFoot);
+  refContainer.appendChild(cardCont);
+  return;
 }
-
 
 //https://nc.iucnredlist.org/redlist/resources/files/1646067752-FINAL_IUCN_Red_List_colour_chart.pdf
 var colorStatus = {
-    "Near threatened (NT)": "#CCE226",
-    "Vulnerable (VU)": "#F9E814",
-    "Endangered (EN)": "#FC7F3F",
-    "Critically Endangered (CR)": "#D81E05",
+  EX: "#000000",
+  EW: "#3d1951",
+  RE: "#5a1a63",
+  CR: "#d3001b",
+  EN: "#fbbf00",
+  VU: "#ffed00",
+  NT: "#fbf2ca",
+  LC: "#78b74a",
+  DD: "#d3d4d5",
+  NA: "#919291",
+  NE: "#ffffff",
 };
