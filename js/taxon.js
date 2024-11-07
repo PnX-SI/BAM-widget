c2f3a1179fbaf8ebd2effcab240a1158b5759246
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2024-11-07 11:43:33 +0100

    Add pgRest connecteur + topTaxon

--- js/taxon.js
+++ js/taxon.js
@@ -70,3 +70,45 @@ function getGbifTaxon(wkt, limit) {
       })
   );
 }
+
+function getPgRestTaxon(wkt, limit = 10, offset = 0) {
+  const geometry = wkt;
+  return (
+    fetch(
+      `https://dev-gtsi.cevennes-parcnational.net/api/rpc/get_taxa_list?in_wkt=${geometry}&in_limit=${limit}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      // Get total number of occurrences
+      .then((data) => {
+        return data.map(({
+          count_occ: occCount,
+          species_name: species,
+          species_key: gbifId,
+          ...rest
+        }) => ({
+          occCount,
+          species,
+          gbifId,
+          ...rest
+        }));
+      })
+  );
+}
+
+function getTopTaxon(wkt) {
+  // let promiseTopTax = new Promise();
+  let promises = [getPgRestTaxon(wkt, 10), getGbifTaxon(wkt, 20)];
+  return Promise.all(promises).then((listOfData) => {
+    const allData = [...listOfData[0], ...listOfData[1]]
+    allData.sort(function (a, b) {
+      return b["occCount"] - a["occCount"];
+    });
+    return Promise.resolve(allData.slice(0, 10))
+  });
+}
+
+// getTopTaxon(wkt).then(data => {
+//   console.log(data)
+// })
