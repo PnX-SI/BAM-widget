c39f89ee8ccbe6f70b9e2cd0ca95a19890ae579a
Cynthia Borot <cynthia.borot@vanoise-parcnational.fr>
2024-11-07 10:55:35 +0100

    feat(taxon): update gbif taxon occurrence retrieval

--- js/taxon.js
+++ js/taxon.js
@@ -1,36 +1,55 @@
-function test() {
-  return fetch("url").then(function (response) {
+function fetchApiTaxonGbif(geometry, limit, offset) {
+  return fetch(
+    `https://api.gbif.org/v1/occurrence/search?geometry=${geometry}&limit=${limit}&offset=${offset}`
+  ).then((response) => {
     return response.json();
   });
 }
 
-function getGbifTaxon(wkt, page) {
-  //   console.log("getGbifTaxon", page);
-  const limit = 300;
-  const offset = page * limit;
-  geometry = wkt;
-  let data = {};
+function getGbifTaxon(wkt, limit) {
+  const geometry = wkt;
+  return (
+    fetch(
+      `https://api.gbif.org/v1/occurrence/search?geometry=${geometry}&limit=1`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      // Get total number of occurrences
+      .then((data) => {
+        return data.count;
+      })
+      .then(async function (countOccurrence) {
+        // Compute the number of pages we need to query
+        const nbOfPages = Math.ceil(countOccurrence / limit);
 
-  let promise = fetch(
-    `https://api.gbif.org/v1/occurrence/search?geometry=${geometry}&limit=${limit}&offset=${offset}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      data.results.forEach((element) => {
-        occCount = (speciesList[element.species] || {})["occCount"] || 0;
-        speciesList[element.species] = {
-          gbifId: element.taxonKey,
-          occCount: occCount + 1,
-        };
-      });
-
-      if (data.endOfRecords == false) {
-        getGbifTaxon(wkt, speciesList, page + 1);
-      }
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+        // Create a promise for each page
+        let promises = [];
+        for (let pageIndex = 0; pageIndex < nbOfPages; pageIndex++) {
+          const offset = pageIndex * limit;
+          promises.push(fetchApiTaxonGbif(geometry, limit, offset));
+        }
+        let speciesList = {};
+        // Run all promises and await for the responses
+        await Promise.all(promises).then((listOfData) => {
+          listOfData
+            .map((apiResult) => {
+              return apiResult.results;
+            })
+            // For each page
+            .forEach((resultsPage) => {
+              // For each occurrence retrieve the gbifID and increase occurrence count
+              resultsPage.forEach((taxonData) => {
+                occCount =
+                  (speciesList[taxonData.species] || {})["occCount"] || 0;
+                speciesList[taxonData.species] = {
+                  gbifId: taxonData.taxonKey,
+                  occCount: occCount + 1,
+                };
+              });
+            });
+        });
+        return speciesList;
+      })
+  );
 }
