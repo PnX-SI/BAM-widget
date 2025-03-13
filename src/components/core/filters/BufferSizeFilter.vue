42f5f2311966eb314ee8c883152e8fd5e4980fc5
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-03-13 10:07:36 +0100

    replace filters by individual components

---
+++ src/components/core/filters/BufferSizeFilter.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  radius: { type: Number, required: true },
+});
+
+const radius = ref(props?.radius);
+
+const emit = defineEmits(["update:radius"]);
+
+watch(radius, () => {
+  emit("update:radius", radius.value);
+});
+</script>
+
+<template>
+  <label for="radius" class="form-label"
+    >{{ $t("bufferSize") }} {{ radius }} km</label
+  >
+  <input
+    type="range"
+    class="form-range"
+    min="1"
+    max="100"
+    v-model="radius"
+    id="radius"
+  />
+</template>
---
+++ src/components/core/filters/DateFilter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, watch } from "vue";
+const props = defineProps({
+  label: { type: String, required: true },
+  id: String,
+  currentDate: Date,
+});
+
+const date = ref(null);
+
+const emit = defineEmits(["update:date"]);
+
+watch(date, () => {
+  emit("update:date", date.value);
+});
+</script>
+<template>
+  <label :for="props.id">{{ props.label }}</label>
+  <input :id="props.id" class="form-control" type="date" v-model="date" />
+</template>
