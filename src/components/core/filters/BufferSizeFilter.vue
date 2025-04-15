aceb1b124165b96c2ee6fdd38dc0c4261547d481
jacquesfize <jacques.fize@gmail.com>
2025-04-15 13:04:51 +0200

    change radius selection component

--- src/components/core/filters/BufferSizeFilter.vue
+++ src/components/core/filters/BufferSizeFilter.vue
@@ -15,15 +15,22 @@ watch(radius, () => {
 </script>
 
 <template>
-  <label for="radius" class="form-label"
-    >{{ $t("bufferSize") }} {{ radius }} km</label
-  >
-  <input
-    type="range"
-    class="form-range"
-    min="1"
-    max="100"
-    v-model="radius"
-    id="radius"
-  />
+  <div class="row">
+    <label for="radius" class="form-label col-8"
+      >{{ $t("bufferSize") }} {{ radius }} km</label
+    >
+    <input
+      type="range"
+      class="form-range"
+      min="1"
+      max="100"
+      v-model="radius"
+      id="radius"
+    />
+  </div>
 </template>
+<style scoped>
+#radius {
+  width: 30%;
+}
+</style>
