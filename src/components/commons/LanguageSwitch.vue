5122c848045e92cbe395244c5b07ae4790d67bd8
jacquesfize <jacques.fize@gmail.com>
2025-04-15 13:01:46 +0200

    add a language switch component

---
+++ src/components/commons/LanguageSwitch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale" class="form-select">
+      <option
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale"
+      >
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+</template>
