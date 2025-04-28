be7abc698b4ab05b385f1ce7d822e5003c8fbf5b
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-04-28 13:43:22 +0200

    externalise search form

---
+++ src/components/commons/SearchForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const searchString = ref("");
+
+const emit = defineEmits(["update:searchString"]);
+
+watch(searchString, () => {
+  emit("update:searchString", searchString.value);
+});
+</script>
+
+<template>
+  <div class="input-group mb-2">
+    <input
+      type="text"
+      class="form-control"
+      id="search"
+      v-model="searchString"
+    />
+    <label for="search" class="input-group-text"
+      ><i class="bi bi-search"></i
+    ></label>
+  </div>
+</template>
