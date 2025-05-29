32c0a1950b064f7ad5f1cc6f5d2e3bda9c169d63
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-05-29 13:17:44 +0200

    Update src/lib/connectors/gbif.js

--- src/lib/connectors/gbif.js
+++ src/lib/connectors/gbif.js
@@ -243,7 +243,7 @@ class GbifConnector extends Connector {
   }
 
   sourceDetailMessage() {
-    return "The number of observations and the dates of the last sightings are based on an aggregation of the last 30,000 observations made within the selected area.";
+    return "The number of observations and the dates of the latest sightings are based on an aggregation of the latest 30,000 observations made within the selected area.";
   }
 }
 
