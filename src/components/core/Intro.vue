b200e55733c2a38aca628b3a134ebbebb62142b5
jacquesfize <jacques.fize@gmail.com>
2025-06-01 15:02:29 +0200

    add collapse button on how to section

--- src/components/core/Intro.vue
+++ src/components/core/Intro.vue
@@ -1,12 +1,25 @@
+<script setup>
+import { ref } from "vue";
+const visible = ref(false);
+</script>
+
 <template>
   <div class="card small">
     <div class="card-header">
-      <h4>{{ $t("howto") }}</h4>
+      <h4>
+        {{ $t("howto") }}
+        <BButton v-b-toggle.collapse-howto variant="light">
+          <i class="bi bi-chevron-down" v-if="!visible"></i>
+          <i class="bi bi-chevron-up" v-else></i>
+        </BButton>
+      </h4>
     </div>
-    <div
-      class="card-body"
-      style="overflow-y: scroll; height: 30vh"
-      v-html="$t('intro')"
-    ></div>
+    <BCollapse id="collapse-howto" v-model="visible">
+      <div
+        class="card-body"
+        style="overflow-y: scroll; height: 30vh"
+        v-html="$t('intro')"
+      ></div>
+    </BCollapse>
   </div>
 </template>
