b065984efdce7f1bfc1151ee8727ce6eaf01b078
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-08-20 11:38:44 +0200

    simplify Intro component

--- src/components/core/Intro.vue
+++ src/components/core/Intro.vue
@@ -1,27 +1,22 @@
 <script setup>
 import { ref } from "vue";
-const visible = ref(false);
 </script>
 
 <template>
-  <div class="card small">
-    <div class="card-header">
-      <h4>
-        {{ $t("howto") }}
-        <BButton v-b-toggle.collapse-howto variant="light">
-          <i class="bi bi-chevron-down" v-if="!visible"></i>
-          <i class="bi bi-chevron-up" v-else></i>
-        </BButton>
-      </h4>
-    </div>
-    <BCollapse id="collapse-howto" v-model="visible">
-      <div class="card-body" style="overflow-y: scroll; height: 30vh">
-        <VueShowdown
-          :markdown="$t('intro')"
-          flavor="github"
-          :options="{ emoji: true }"
-        />
-      </div>
-    </BCollapse>
+  <div id="intro">
+    <h2>{{ $t("introTitle") }}</h2>
+    <VueShowdown
+      :markdown="$t('intro')"
+      flavor="github"
+      :options="{ emoji: true }"
+    />
   </div>
 </template>
+<style>
+#intro {
+  padding: 1em;
+  background-color: #198754;
+  border-radius: 2%;
+  color: white;
+}
+</style>
