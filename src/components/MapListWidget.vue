e156a69fa8bd4d98f1f6de933b719511e9d9081f
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-10-20 16:10:53 +0200

    first version of the PWA of BAM

--- src/components/MapListWidget.vue
+++ src/components/MapListWidget.vue
@@ -1,7 +1,7 @@
 <script setup>
-    // Components
     import TaxonList from '@/components/core/taxonList/TaxonList.vue';
     import Map from './core/map/Map.vue';
+    import { computed, ref } from 'vue';
 
     const props = defineProps({
         height: {
@@ -9,15 +9,52 @@
             default: '100vh',
         },
     });
+
+    const windowWidth = ref(window.innerWidth);
+
+    const showMap = ref(false);
+
+    const isMobile = computed(() => {
+        return windowWidth.value <= 768;
+    });
+
+    function onResize() {
+        windowWidth.value = window.innerWidth;
+    }
+
+    window.addEventListener('resize', onResize);
 </script>
 
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid position-relative">
+        <!-- Bouton visible seulement sur mobile -->
+        <button
+            class="btn btn-primary d-lg-none d-md-none position-fixed"
+            style="
+                top: 70px;
+                left: 50%;
+                transform: translateX(-50%);
+                z-index: 1000;
+            "
+            @click="showMap = !showMap"
+        >
+            {{ showMap ? 'Voir la liste' : 'Voir la carte' }}
+        </button>
+
         <div class="row">
-            <div class="col-12 col-lg-6 col-md-6">
-                <Map :height="props.height" />
+            <!-- Carte -->
+            <div class="col-12 col-lg-6 col-md-6" v-show="showMap || !isMobile">
+                <Map
+                    :height="isMobile ? '100vh !important' : props.height"
+                    @update-geom="() => (showMap = false)"
+                />
             </div>
-            <div class="col-12 col-lg-6 col-md-6">
+
+            <!-- Liste -->
+            <div
+                class="col-12 col-lg-6 col-md-6"
+                v-show="!showMap || !isMobile"
+            >
                 <TaxonList :style="'height:' + props.height" />
             </div>
         </div>
