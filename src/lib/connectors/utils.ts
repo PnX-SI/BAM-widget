e2d1bd93b4fb724f61bff185f29d3cdb1c531d29
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-12-01 00:07:37 +0100

    Limit the number of coordinates of geometries sent to the GBIF search endpoint (#102)

--- src/lib/connectors/utils.ts
+++ src/lib/connectors/utils.ts
@@ -2,6 +2,7 @@ import { CONNECTORS } from './connectors';
 import { GbifConnector } from './gbif';
 import { GeoNatureConnector } from './geonature';
 import { Connector } from './connector';
+import { simplify } from '@turf/turf';
 
 type ConnectorParams = Record<string, any>;
 
@@ -19,4 +20,35 @@ function getConnector(
     }
 }
 
-export { getConnector };
+/**
+ * Simplify a polygon by removing points while keeping the total number of points below a certain threshold (190).
+ * The simplification is done by the turf.js simplify algorithm.
+ * @param {Object} polygon - Polygon to be simplified.
+ * @param {number} [tolerance=0.01] - Tolerance for simplification.
+ * @param {number} [threshold=190] - Threshold for total number of points.
+ * @returns {Object} - Simplified polygon.
+ */
+function simplifyPolygon(polygon, tolerance = 0.01, threshold = 190) {
+    let simplified = simplify(polygon, { tolerance });
+    let totalCoordinatesCount = getTotalCoordinatesCount(simplified);
+    while (totalCoordinatesCount > threshold) {
+        simplified = simplify(polygon, { tolerance });
+        totalCoordinatesCount = getTotalCoordinatesCount(simplified);
+    }
+    return simplified;
+}
+
+function getTotalCoordinatesCount(polygon) {
+    let totalCoordinatesCount = 0;
+    if (polygon.type === 'MultiPolygon') {
+        polygon.coordinates.forEach((multiPolygon) => {
+            multiPolygon.forEach((polygon) => {
+                totalCoordinatesCount += polygon.length;
+            });
+        });
+    } else {
+        totalCoordinatesCount += polygon.coordinates.length;
+    }
+    return totalCoordinatesCount;
+}
+export { getConnector, simplifyPolygon };
