d1fce7cef5bd802dcaa39f1113299f0b76d6d8d0
jacquesfize <jacques.fize@gmail.com>
2026-06-14 14:52:47 +0200

    feat: add environment configuration and update Vite setup to use env variables

--- vite.config.js
+++ vite.config.js
@@ -1,79 +1,82 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import Components from 'unplugin-vue-components/vite';
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
 import { VitePWA } from 'vite-plugin-pwa';
 import webfontDownload from 'vite-plugin-webfont-dl';
-
 // https://vite.dev/config/
-export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/BAM-widget/' : '/',
-    plugins: [
-        vue(),
-        webfontDownload([
-            'https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@300;400;500;600;700&display=swap',
-        ]),
-        vueDevTools(),
-        Components({
-            resolvers: [BootstrapVueNextResolver()],
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: [
-                'favicon.ico',
-                'apple-touch-icon-180x180.png',
-                'maskable-icon-512x512.png',
-            ],
-            manifest: {
-                name: 'BAM (Biodiversity Around Me',
-                short_name: 'BAM',
-                description:
-                    'BAM is an application for showing species seen nearby any location around the world',
-                theme_color: '#ffffff',
-                permissions: ['geolocation'],
-                icons: [
-                    {
-                        src: 'manifest-icon-192.maskable.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        purpose: 'any',
-                    },
-                    {
-                        src: 'manifest-icon-192.maskable.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
-                    {
-                        src: 'manifest-icon-512.maskable.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any',
-                    },
-                    {
-                        src: 'manifest-icon-512.maskable.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.BASEPATH || '/',
+        plugins: [
+            vue(),
+            webfontDownload([
+                'https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@300;400;500;600;700&display=swap',
+            ]),
+            vueDevTools(),
+            Components({
+                resolvers: [BootstrapVueNextResolver()],
+            }),
+            VitePWA({
+                registerType: 'autoUpdate',
+                includeAssets: [
+                    'favicon.ico',
+                    'apple-touch-icon-180x180.png',
+                    'maskable-icon-512x512.png',
                 ],
+                manifest: {
+                    name: 'BAM (Biodiversity Around Me',
+                    short_name: 'BAM',
+                    description:
+                        'BAM is an application for showing species seen nearby any location around the world',
+                    theme_color: '#ffffff',
+                    permissions: ['geolocation'],
+                    icons: [
+                        {
+                            src: 'manifest-icon-192.maskable.png',
+                            sizes: '192x192',
+                            type: 'image/png',
+                            purpose: 'any',
+                        },
+                        {
+                            src: 'manifest-icon-192.maskable.png',
+                            sizes: '192x192',
+                            type: 'image/png',
+                            purpose: 'maskable',
+                        },
+                        {
+                            src: 'manifest-icon-512.maskable.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'any',
+                        },
+                        {
+                            src: 'manifest-icon-512.maskable.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'maskable',
+                        },
+                    ],
+                },
+                workbox: {
+                    navigateFallbackDenylist: [
+                        /\/docs?(\/.*)?$/, // exclut tout ce qui commence par /docs
+                    ],
+                },
+                devOptions: {
+                    enabled: true,
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
-            workbox: {
-                navigateFallbackDenylist: [
-                    /\/docs?(\/.*)?$/, // exclut tout ce qui commence par /docs
-                ],
-            },
-            devOptions: {
-                enabled: true,
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-    },
+    };
 });
